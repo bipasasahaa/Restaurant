e720d3e903997012529d64da17d414273ab4095a
bipasasahaa <110908424+bipasasahaa@users.noreply.github.com>
2023-10-19 23:09:26 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
+
+
+module.exports = {
+
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
+  };
+  
+  
